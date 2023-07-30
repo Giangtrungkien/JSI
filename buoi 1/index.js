e9aff8c2adef21426623139ba9aa9b634bc6559a
Giangtrungkien <giangtrungkien2008@gmail.com>
2023-07-30 15:41:39 +0700

    bai hoc buoi 1

--- buoi 1/index.js
+++ buoi 1/index.js
@@ -1,5 +1,23 @@
-const getPosts = async () => {
-  const url = "https://jsonplaceholder.typicode.com/posts";
+// const getPosts = async () => {
+//   const url = "https://jsonplaceholder.typicode.com/posts";
+//   const res = await fetch(url);
+//   const data = await res.json();
+//   const main = document.querySelector("#main");
+//   let html = "";
+//   for (let i = 0; i < data.length; i++) {
+//     var id = data[i].id;
+//     var title = data[i].title;
+//     var body = data[i].body;
+//     html += `
+//         <h1>${id}</h1>
+//         <h2>${title}</h2>
+//         <p>${body}</p>`;
+//   }
+//   main.innerHTML = html;
+// };
+// getPosts();
+const getPhotos = async () => {
+  const url = "https://jsonplaceholder.typicode.com/photos";
   const res = await fetch(url);
   const data = await res.json();
   const main = document.querySelector("#main");
@@ -7,10 +25,12 @@ const getPosts = async () => {
   for (let i = 0; i < data.length; i++) {
     var id = data[i].id;
     var title = data[i].title;
-    var body = data[i].body;
+    var img = data[i].url;
     html += `
         <h1>${id}</h1>
         <h2>${title}</h2>
-        <p>${body}</p>`;
+        <img src="${img}" alt=""/>`;
   }
+  main.innerHTML = html;
 };
+getPhotos();
